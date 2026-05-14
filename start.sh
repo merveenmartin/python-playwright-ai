908f736e01b644b6a5f7eb392f7bc1f13f7bcc81
merveen <merveen.martin@gmail.com>
2026-05-14 14:54:38 -0500

    commented the aks code

--- start.sh
+++ start.sh
@@ -1,22 +1,22 @@
-#!/bin/bash
-set -e
+# #!/bin/bash
+# set -e
 
-AZP_AGENT_NAME=${AZP_AGENT_NAME:-$(hostname)}
+# AZP_AGENT_NAME=${AZP_AGENT_NAME:-$(hostname)}
 
-./config.sh \
-  --unattended \
-  --url "$AZP_URL" \
-  --auth pat \
-  --token "$AZP_TOKEN" \
-  --pool "$AZP_POOL" \
-  --agent "$AZP_AGENT_NAME" \
-  --replace \
-  --acceptTeeEula
+# ./config.sh \
+#   --unattended \
+#   --url "$AZP_URL" \
+#   --auth pat \
+#   --token "$AZP_TOKEN" \
+#   --pool "$AZP_POOL" \
+#   --agent "$AZP_AGENT_NAME" \
+#   --replace \
+#   --acceptTeeEula
 
-cleanup() {
-  ./config.sh remove --unattended --auth pat --token "$AZP_TOKEN"
-}
+# cleanup() {
+#   ./config.sh remove --unattended --auth pat --token "$AZP_TOKEN"
+# }
 
-trap cleanup EXIT
+# trap cleanup EXIT
 
-./run.sh
+# ./run.sh
